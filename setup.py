d6a70de126ada930f9cda049747e264b2dcb2cf2
Clément Besnier <clemsciences@aol.com>
2021-01-23 23:52:36 +0100

    Version 1.0.2 - added dependencies for pip

--- setup.py
+++ setup.py
@@ -3,15 +3,30 @@ import os
 
 from setuptools import find_packages, setup
 
-
 CURDIR = os.path.abspath(os.path.dirname(__file__))
+
+DEPENDENCIES = ["beautifulsoup4==4.9.1",
+                "bs4==0.0.1",
+                "cltk==0.1.111",
+                "greek-accentuation==1.2.0",
+                "jupyter",
+                "lxml",
+                "matplotlib",
+                "networkx==2.4",
+                "norsecorpus==1.0.2",
+                "notebook==6.1.5",
+                "numpy==1.19.1",
+                "requests==2.24.0",
+                "scikit-learn==0.23.1",
+                "scipy==1.4.1",
+                "sigurd==1.2.9"]
 
 with io.open(os.path.join(CURDIR, "README.md"), "r", encoding="utf-8") as f:
     README = f.read()
 
 setup(
     name='latechclfl2020besnier',
-    version='1.0.1',
+    version='1.0.2',
     url='https://www.clementbesnier.fr/dlh-vol-nib',
     license='Creative Commons Attribution 4.0 International Licence',
     author='Clément Besnier',
@@ -24,6 +39,7 @@ setup(
     keywords=["old-norse", "middle-high-german", "latin", "network analysis"],
     scripts=[],
     zip_safe=True,
+    install_requires=DEPENDENCIES,
     test_suite="tests.test_project",
     python_requires=">=3.6",
     # license and classifier list:
